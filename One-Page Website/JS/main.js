e15c895f68af741972bf341626bc89b021463310
Pcharlot13 <43765007+Pcharlot13@users.noreply.github.com>
2021-09-16 15:12:09 -0400

    uodate
    main

--- One-Page Website/JS/main.js
+++ One-Page Website/JS/main.js
@@ -1,26 +1,41 @@
+// Open the Modal
+function openModal() {
+  document.getElementById("myModal").style.display = "block";
+}
 
-// creates a div and an id called lightbox, basically creates the lightbox so that we can edit it in css and iterate on it in js
-const lightbox = document.createElement('div')
-lightbox.id = 'lightbox'
-document.body.appendChild(lightbox)
+// Close the Modal
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
 
-//Selecting all images in our html file and checking that if we click it
-const images = document.querySelectorAll('img')
-images.forEach(image => {
-  image.addEventListener('click', e => {
-      //when we click an image it turns on our lightbox which displays that image
-    lightbox.classList.add('active')
-    const img = document.createElement('img')
-    img.src = image.src
-    //this keeps more than one image from appearing, so we only view one image at a time
-    while (lightbox.firstChild) {
-      lightbox.removeChild(lightbox.firstChild)
-    }
-    lightbox.appendChild(img)
-  })
-})
-// This is so we can click out of our lightbox
-lightbox.addEventListener('click', e => {
-  if (e.target !== e.currentTarget) return
-  lightbox.classList.remove('active')
-})
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  //
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
